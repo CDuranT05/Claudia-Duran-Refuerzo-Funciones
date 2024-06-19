4314fac7d43aa958ebfae22fcbe4fd68ea514ce6
Claudia <claudiadurantupano@gmail.com>
2024-06-19 13:19:55 -0400

    Lanzamiento-De-Dados
    ejercicio donde se unen funciones y ciclos para lograr un juego de lanzamiento de dados

---
+++ Lanzamiento de Dados/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string LanzarDados()
+{
+    Random random = new Random();
+    int resultados = random.Next(0,7);
+    string Dados= resultados.ToString();
+    return Dados;
+}
+
+
+
+bool jugar = true;
+
+while (jugar)
+{
+    Console.WriteLine("Quieres Lanzar los Dados?");
+    Console.WriteLine("L:para lanzar / S:salir");
+    string usuario = Console.ReadLine();
+
+    if (usuario == "L")
+    {
+        
+        Console.WriteLine(value:"el primer resultado del lanzamiento es: " + LanzarDados());
+        Console.WriteLine(value: "el segundo resultado del lanzamiento es: " + LanzarDados());
+    }
+    else if(usuario == "S")
+    {
+        jugar = false; break;
+    }
+}
